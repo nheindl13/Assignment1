cf13ba8af012d548132c9532b903e8d4d402c6e4
Nick Heindl <nheindl13@gmail.com>
2013-06-23 21:14:34 -0300

    Update web.js

--- web.js
+++ web.js
@@ -1,8 +1,6 @@
-#!/usr/bin/env node 
-
+#!/usr/bin/env node
 var fs = require('fs');
-var file = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
-
-fs.writeFileSync(file, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + file);
+var outfile = "hello.txt";
+var out = "Modify this script to write out something different.\n";
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
